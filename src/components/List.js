a4eedf82f945d7e0e0e9f4e81f8b3269c0bfa2b6
Aan Alma <aanalma03@gmail.com>
2024-04-26 11:07:40 +0700

    create contact content

--- src/components/List.js
+++ src/components/List.js
@@ -1,9 +1,81 @@
 import Content from "./Content"
+import styles from '../style/List.module.css'
+import Icon from "./Icon"
 
 const Contact = () => {
     return (
         <Content>
-            <h3>List</h3>
+            <div className={styles.header}>
+                <div className={styles.headerText}>
+                    <h3>Contacts</h3>
+                    <p>Search new friends and chat conversation with them. Please click the profile to see more information.</p>
+                </div>
+                <form>
+                    <input type="text" placeholder="Search by name"/>
+                </form>           
+            </div>
+            <div className={styles.mainContent}>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Sandhika galih</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Udin Sitompul</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Muhammad Sumbul Alfariq</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+                <div className={styles.mainList}>
+                    <Icon name="profile" size="50px"/>
+                    <p>Name</p>
+                </div>
+            </div>
         </Content>
     )
 }
